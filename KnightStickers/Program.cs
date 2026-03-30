35c00b8c46d3e1cfdd0dc7325bf54148a4a09412
Dr.Charlatan <sahsasimonenko@gmail.com>
2026-03-30 14:56:44 +0300

    fix

--- KnightStickers/Program.cs
+++ KnightStickers/Program.cs
@@ -24,7 +24,7 @@ class Program
     {
         // === НАСТРОЙКИ РАЗМЕРОВ ===
         // Измени эти значения под свои реальные размеры
-        double stickerWidthMm = 22.0;
+        double stickerWidthMm = 21.0;
         double stickerHeightMm = 40.0;
         double distanceBetweenMm = 4.0;
 
